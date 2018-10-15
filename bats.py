88258590be3d8edf601df1cb7ef1997cbe9640fb
Rachel Ramsay <rachel.ramsay@outlook.com>
2018-10-15 12:14:35 -0700

    More error handling

--- bats.py
+++ bats.py
@@ -7,6 +7,8 @@ import json
 import logging
 import os
 import pathlib
+import random
+import sys
 
 import requests
 from flickrapi import FlickrAPI
@@ -99,10 +101,15 @@ params = {"text": search_terms,
           "extras": "license,date_upload,owner_name,url_z,url_c,url_l,url_o",
           "sort": "relevance",
           "per_page": "10"}
-resp = flickr.photos.search(**params)
-photos = resp["photos"]["photo"]
+try:
+    resp = flickr.photos.search(**params)
+    photos = resp["photos"]["photo"]
+except (requests.exceptions.ConnectionError, KeyError) as e:
+    logger.exception(e)
+    sys.exit()
 
 # Turn Flickr results into entities.
+entities = list()
 for photo in photos:
     kind = "Photo"
     name = "Flickr-" + photo.get("id")
@@ -111,7 +118,6 @@ for photo in photos:
     entity.update({
         "source": "Flickr",
         "search_terms": search_terms,
-        "last_tweeted": datetime.datetime.utcfromtimestamp(1514764800),  # 1/1/18
         "is_classified": False
     })
     for k, v in photo.items():
@@ -128,24 +134,32 @@ for photo in photos:
     # Download image. (You can use Cloud Vision API with a remote image, but
     # it's flaky.)
     name = entity.key.name
-    filepath = download_image(url=entity.get("download_url"),
-                              name=name)
+    try:
+        filepath = download_image(url=entity.get("download_url"),
+                                  name=name)
+    except requests.exceptions.HTTPError as e:
+        logger.exception(e)
+        continue
     
     # Load image.
-    logger.debug(f"Opening {filepath}...")
+    logger.debug(f"Opening {name}...")
     with io.open(filepath, 'rb') as image_file:
         content = image_file.read()
     image = vision.types.Image(content=content)
 
     # Label image as a whole and find objects in image.
-    logger.debug(f"Requesting image annotation for {filepath}...")
-    response = v_client.annotate_image({
-        "image": image,
-        "features": [{'type': vision.enums.Feature.Type.OBJECT_LOCALIZATION},
-                     {'type': vision.enums.Feature.Type.LABEL_DETECTION}]
-    })
-    logger.debug(f"Response for {filepath} annotation request: {response}")
-
+    logger.info(f"Starting classification for for {name}...")
+    try:
+        response = v_client.annotate_image({
+            "image": image,
+            "features": [{'type': vision.enums.Feature.Type.OBJECT_LOCALIZATION},
+                        {'type': vision.enums.Feature.Type.LABEL_DETECTION}]
+        })
+        logger.debug(f"Response for {name} annotation request: {response}")
+    except exceptions.GoogleAPIError as e:
+        logger.exception(e)
+        continue
+    
     labels = set()
     crop_boxes = set()
     if response.label_annotations:
@@ -170,7 +184,6 @@ for photo in photos:
     if not is_a(["bat"], labels) and crop_boxes:
         logger.debug(f"Cropping {name}...")
         im = Image.open(filepath)
-        logger.debug(crop_boxes)
         for cb in crop_boxes:
             with im.crop(box=cb) as im2:
                 # Convert PIL.Image.Image to bytes.
@@ -179,8 +192,12 @@ for photo in photos:
                     byts = buffer.getvalue()
                     img = vision.types.Image(content=byts)
             logger.debug(f"Requesting label detection for crop...")
-            response = v_client.label_detection(image=img)
-            logger.debug(f"Response for crop label detection request: {response}")
+            try:
+                response = v_client.label_detection(image=img)
+                logger.debug(f"Response for crop label detection request: {response}")
+            except exceptions.GoogleAPIError as e:
+                logger.exception(e)
+                continue
             if response.label_annotations:
                 labels.update([a.description for a in response.label_annotations])
                 if is_a(["bat"], labels):
@@ -188,11 +205,15 @@ for photo in photos:
                     break
         im.close()
         logger.debug("Done cropping {name}.")
-       
+    
     logger.debug(f"{name}'s labels: {labels}")
-    entity.update({"is_bat": is_a(["bat"], labels),
-                   "vision_labels": json.dumps(list(labels))})
-    logger.info(f"\n\n{name} is_bat = {entity.get('is_bat')}!\n\n")
+    entity.update({
+        "is_bat": is_a(["bat"], labels),
+        "vision_labels": json.dumps(list(labels)),
+        "is_classified": True
+    })
+    logger.info(f"{name} is_bat = {entity.get('is_bat')}!\n\n")
+    entities.append(entity)
     
     # We've done what we can -- upsert that entitity! (We could also batch
     # these, or use a transaction if we needed to perform get and update
@@ -201,3 +222,22 @@ for photo in photos:
         ds_client.put(entity)
     except exceptions.GoogleAPIError as e:
         logger.exception(e)
+        continue
+
+# Get bats from Cloud Datastore.
+logger.info(f"Retrieving is_bat entities...")
+query = ds_client.query(kind="Photo")
+query.add_filter("is_bat", "=", True)
+# If you don't want to load a potentially huge number of entities into memory,
+# you can make the query keys_only and then get a single entitity by key.
+try:
+    entities = list(query.fetch())
+    logger.debug(entities)
+except exceptions.GoogleAPIError as e:
+    logger.exception(e)
+
+# We made a list of entities up above, so even if retrieving them from Cloud
+# Datastore failed, they're still in memory.
+entity = random.choice(entities)
+
+# I can't believe it's finally time to tweet.
