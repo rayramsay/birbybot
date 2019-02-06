00408d8f8a01a9c9c05c7435c7fd4455ce847fc2
Rachel Ramsay <rachel.ramsay@outlook.com>
2019-02-06 10:59:33 -0800

    Change test case to be easier to read

--- test_classify_images.py
+++ test_classify_images.py
@@ -6,13 +6,16 @@ import pytest
 from classify_images import is_safe
 
 def test_is_safe():
-    assert is_safe({'adult': 'VERY_UNLIKELY',
-                    'medical': 'UNLIKELY',
-                    'spoofed': 'POSSIBLE',
-                    'violence': 'VERY_UNLIKELY',
-                    'racy': 'VERY_UNLIKELY'}) == True
+    """`is_safe` should return False if any categories are LIKELY or VERY_LIKELY;
+    conversely, `is_safe` should return True if all categories are VERY_UNLIKELY,
+    LIKELY, or POSSIBLE."""
     assert is_safe({'adult': 'VERY_UNLIKELY',
                     'medical': 'UNLIKELY',
                     'spoofed': 'POSSIBLE',
                     'violence': 'LIKELY',
                     'racy': 'VERY_LIKELY'}) == False
+    assert is_safe({'adult': 'VERY_UNLIKELY',
+                    'medical': 'UNLIKELY',
+                    'spoofed': 'POSSIBLE',
+                    'violence': 'UNLIKELY',
+                    'racy': 'VERY_UNLIKELY'}) == True
